a3bb543802e0f9cbd679aab036924a86b640e90f
Esmeralda Enriquez <78413520+kimmurielO@users.noreply.github.com>
2021-02-15 12:06:17 +0100

    Add files via upload

--- mostrarDP.php
+++ mostrarDP.php
@@ -32,28 +32,37 @@ $bd = new MiBD();
 
 $resultado = $bd->query("SELECT * FROM Almacen");
 
-//var_dump($resultado->fetchArray());
-
 echo "<table>";
 echo "<tr>";
 echo "<th> Tipo de producto </th>";
 echo "<th> Codigo de barras </th>";
 echo "<th> Cantidad inicial </th>";
-echo "<th> Cantidad actual </th>";
 echo "<th> Marca </th>";
 echo "<th> Proveedor </th>";
 echo "</tr>";
 echo "<br>";
 
 while ($row = $resultado->fetchArray()) {
-    echo "<tr>";
+    $CantidadActual = $bd->query("SELECT CantidadActual FROM Recordar WHERE CodigoDeBarras='$row[1]'");
+    $CantidadActual2 = $CantidadActual-> fetchArray();
+    $CantidadAviso = $bd->query("SELECT RecordarCant FROM Recordar WHERE CodigoDeBarras='$row[1]'");
+    $CantidadAviso2 =  $CantidadAviso -> fetchArray();
+
+    if ($CantidadActual2[0] < $CantidadAviso2[0]){
+        echo "<tr class='CantRojo'>";
+    }
+    else{
+        echo "<tr class='CantVerde'>";
+    }
+
+    //echo "<tr>";
     echo "<td> $row[0] </td>";
     echo "<td> $row[1] </td>";
     echo "<td> $row[2] </td>";
     echo "<td> $row[3] </td>";
     echo "<td> $row[4] </td>";
-    echo "<td> $row[5] </td>";
     echo "</tr>";
+
 }
 echo "</table>";
 
